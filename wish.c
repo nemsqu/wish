a12d9528aa9d02ba96a96bf35f82540bb0767982
nemsqu <nelli.kemi@gmail.com>
2022-04-15 21:34:24 +0300

    fixes to path

--- wish.c
+++ wish.c
@@ -10,8 +10,13 @@ struct command {
     char *outputFile;
     struct command *pNext;
 };
-
 typedef struct command COMMAND;
+
+struct path {
+    char path[264];
+    struct path *pNext;
+};
+typedef struct path PATH;
 
 COMMAND *add_command_to_list(COMMAND *pCommand, char **params, int commandNro, char *outputFile){
     printf("ADDING\n");
@@ -53,7 +58,7 @@ COMMAND *add_command_to_list(COMMAND *pCommand, char **params, int commandNro, c
     return pCommand;
 }
 
-COMMAND *free_memory(COMMAND *pCommand){
+COMMAND *empty_commands(COMMAND *pCommand){
     COMMAND *ptr;
 
     while(pCommand != NULL){
@@ -65,8 +70,66 @@ COMMAND *free_memory(COMMAND *pCommand){
         free(ptr->params);
         free(ptr);
     }
+    
     return pCommand;
 }
+
+
+PATH *empty_paths(PATH *path){
+    PATH *ptr;
+
+    while(path != NULL){
+        ptr = path->pNext;
+        free(path);
+        path = ptr;
+    }
+}
+
+PATH *add_paths(char *buffer, PATH *path){
+    //remove newline so that it doesn't mess up the (last) path
+    buffer[strcspn(buffer, "\n")] = 0;
+
+    //cut away command "path"
+    strtok(buffer, " ");
+    char *newPath = strtok(NULL, " ");
+    PATH *ptr;
+
+    if(newPath == NULL){
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+    } else {
+        if(path != NULL){
+            path = empty_paths(path);
+        }
+        while(newPath != NULL){
+            PATH *pNew = NULL;
+            if((pNew = malloc(sizeof(PATH))) == NULL){
+                //ERROR
+            }
+            strcpy(pNew->path, newPath);
+            printf("%ld\n", strlen(newPath));
+            pNew->pNext = NULL;
+
+            if(path == NULL){
+                path = pNew;
+            } else {
+                ptr = path;
+                while(ptr->pNext != NULL){
+                    ptr = ptr->pNext;
+                }
+                ptr->pNext = pNew;
+            }
+            newPath = strtok(NULL, " ");
+        }
+    }
+    /*int z = 0;
+    while(z < paths){
+        printf("%s\n", path[z]);
+        z++;
+    }*/
+    return path;
+}
+
 
 void run_shell(){
     char *buffer = NULL;
@@ -77,47 +140,36 @@ void run_shell(){
     pid_t pid;
     int status;
     char *defaultPath = "/bin";
-    char **path = NULL;
+    PATH *path = NULL;
     COMMAND *pCommand = NULL;
 
     printf("Wish> ");
-    path = malloc(sizeof(char *));
-    path[0] = defaultPath;
+    path = malloc(sizeof(PATH)); // TODO: ADD ERROR CHECK
+    strcpy(path->path, defaultPath);
+    path->pNext = NULL;
     while((read = getline(&buffer, &len, stdin)) != -1){
 
-        //check buil-in commands
+        //check built-in commands
         char command[5] = "";
         strncpy(command, buffer, 4);
         command[4] = '\0';
         if(strcmp(command, "path") == 0){
-            int paths = 0;
-            //cut away command "path"
-            strtok(buffer, " ");
-            char *newPath = strtok(NULL, " ");
-
-            if(newPath == NULL){
-                char error_message[30] = "An error has occurred\n";
-                write(STDERR_FILENO, error_message, strlen(error_message));
-            } else {
-                while(newPath != NULL){
-                    paths++;
-                    path = realloc(path, paths*sizeof(char *)); // TODO error check
-                    printf("realloc 1\n");
-                    path[paths-1] = newPath;
-                    newPath = strtok(NULL, " ");
-                }
-            }
-            /*int z = 0;
-            while(z < paths){
-                printf("%s\n", path[z]);
-                z++;
-            }*/
+            printf("Path?\n");
+            path = add_paths(buffer, path);
             printf("Wish> ");
             continue;
         }
         if(strcmp(command, "exit") == 0){
+            empty_commands(pCommand);
+            if(params != NULL){
+                free(params);
+            }
+            if(path != NULL){
+                path = empty_paths(path);
+            }
             exit(0);
         }
+        
         char cdCommand[3] = "";
         strncpy(cdCommand, command, 2);
         cdCommand[2] = '\0';
@@ -170,9 +222,9 @@ void run_shell(){
                 tok = strtok(NULL, " ");
                 continue;
             }
-            printf("realloc next new %ld old %ld\n", (i+1)*sizeof(char *), sizeof(params));
+            //printf("realloc next new %ld old %ld\n", (i+1)*sizeof(char *), sizeof(params));
             params = realloc(params, (i+1)*sizeof(char *)); //TODO error handling
-            printf("realloc 2\n");
+            //printf("realloc 2\n");
             params[i-1] = tok;
             i++;
             tok = strtok(NULL, " ");
@@ -209,73 +261,22 @@ void run_shell(){
                     write(STDERR_FILENO, error_message, strlen(error_message));
                 }
                 case 0: {
-                    /*printf("i = %d\n", i-1);
-                    int beginning = 0, end = 0, hitCount = 0, rounds = 0;
-                    while(hitCount <=nPid+1 && end<(i-1) && rounds < 10){
-                        printf("hitcount %d nPid %d end %d i %d\n", hitCount, nPid, end, i);
-                        while(end<(i-1) && hitCount < nPid +1){
-                            if(strcmp(params[end], "&") == 0){
-                                printf("********* HIT ********* end: %d next: %s\n", end, params[end+1]);
-                                hitCount++;
-                                if(hitCount == nPid){
-                                    beginning = end;
-                                    printf("beginning = %d\n", beginning);
-                                } else if(hitCount == nPid+1){
-                                    break;
-                                }
-                            }
-                            //printf("end %d i %d\n", end, i);
-                            end++;
-                            //printf("end %d i-1 %d\n", end, i-1);
-                        }
-                        //printf("Here2 rounds %s\n", params[end]);
-                        rounds++;
-                        if(params[end] == NULL){
-                            printf("null\n");
-                            break;
-                        } else {
-                            printf("Params end+1 %s\n", params[end+1]);
-                        }
-                    }
-                    printf("end: %d\n", end);
-                    printf("looping done\n");
-                    if(hitCount != 0){
-                        commandParams = malloc((end-beginning+1)*sizeof(char *)); //TODO: error handling
-                        printf("end %d beginning %d\n", end, beginning);
-                        int k = 0;
-                        while(k+ beginning < end){
-                            commandParams[k] = params[k + beginning];
-                            k++;
-                        }
-                        //memcpy(commandParams, &params[beginning], (end-beginning)*sizeof(*params));
-                        printf("memcpy done\n");
-                        commandParams[k] = NULL;
-                        printf("moi");
-                    } else {
-                        commandParams = &params[0];
-                    }
-                    
-                    int l = 0;
-                    while(l <= end){
-                        printf("%d %s", l, commandParams[l]);
-                        l++;
-                    }
-                    printf("%s\n", commandParams[end-beginning]);*/
                     COMMAND *wanted = pCommand;
 
                     wanted = pCommand;
                     while(wanted->number != nPid+1){
-                        printf("Loopissa: %d\n", wanted->number);
+                        //printf("Loopissa: %d\n", wanted->number);
                         wanted = wanted->pNext;
                     }
 
                     int available = 0;
                     char fullPath[264] = "";
-                    int x = 0;
-                    while(path[x] != NULL){
+                    PATH *pathptr = path;
+                    while(pathptr != NULL){
                     //for (int x = 0; x < strlen(path); x++){
+                        printf("Path[x] = %s\n", pathptr->path);
                         strcpy(fullPath, "");
-                        strcat(fullPath, path[x]);
+                        strcat(fullPath, pathptr->path);
                         //fullPath = realloc(fullPath, sizeof(fullPath) + sizeof("/"));
                         strcat(fullPath, "/");
                         //fullPath = realloc(fullPath, sizeof(fullPath) + sizeof(params[0]));
@@ -285,7 +286,7 @@ void run_shell(){
                             available = 1;
                             break;
                         }
-                        x++;
+                        pathptr = pathptr->pNext;
                     }
                     printf("%d\n", available);
                     if(available == 0){
@@ -327,16 +328,10 @@ void run_shell(){
             n--;
         }
 
-        
-        if(pCommand == NULL){
-            printf("should not happen");
-        }
-
-        pCommand = free_memory(pCommand);
+        pCommand = empty_commands(pCommand);
         
         printf("Memory freed\n");
         printf("Wish> ");
-        free(params);
     }
 }
 
